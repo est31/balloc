8da59081c805ed0fb8a7c74569a7d2f330261760
est31 <MTest31@outlook.com>
2019-03-03 22:46:32 +0100

    Rename

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,12 @@ use std::mem::size_of;
 
 pub struct AllocError;
 
-pub struct BVec<B :AllocBound, T> {
+pub struct Bvec<B :AllocBound, T> {
 	inner :Vec<T>,
 	bound :B,
 }
 
-impl<B :AllocBound, T> BVec<B, T> {
+impl<B :AllocBound, T> Bvec<B, T> {
 	pub fn with_capacity(mut bound :B, capacity :usize) -> Result<Self, AllocError> {
 		try!(bound.try_alloc(capacity * size_of::<T>()));
 		Ok(Self {
