b8228cb48005dabfa5b8aa2ca1ff9b42bea470de
est31 <MTest31@outlook.com>
2019-03-04 00:43:21 +0100

    Add Unbounded struct

--- src/bounds.rs
+++ src/bounds.rs
@@ -4,3 +4,14 @@ pub trait AllocBound {
 	fn try_alloc(&mut self, amount :usize) -> Result<(), AllocError>;
 	fn dealloc(&mut self, amount :usize);
 }
+
+pub struct Unbounded;
+
+impl AllocBound for Unbounded {
+	fn try_alloc(&mut self, amount :usize) -> Result<(), AllocError> {
+		Ok(())
+	}
+	fn dealloc(&mut self, amount :usize) {
+		// Nothing to do.
+	}
+}
