d202e66c31ef370b7dbf383c0b4e3dff50a782fb
est31 <MTest31@outlook.com>
2019-03-08 02:33:08 +0100

    Expand tests

--- tests/simple.rs
+++ tests/simple.rs
@@ -6,10 +6,16 @@ use balloc::{Bvec, NumberBounded};
 fn test_bvec_ops() {
 	let b = NumberBounded::new_wrapped(42);
 	let mut v = Bvec::with_capacity(b, 42).unwrap();
-	v.push(1u8).unwrap();
+	v.push(0u8).unwrap();
 	v.push(2u8).unwrap();
 	v.push(3u8).unwrap();
 	v.push(4u8).unwrap();
-	assert_eq!({ let sl :&[_] = &v[..]; sl }, &[1, 2, 3, 4]);
+	// Test for Index + IndexMut implementations
+	assert_eq!(v[0], 0);
+	v[0] = 1;
+	assert_eq!(v[0], 1);
+	// Test for Index implementation
+	assert_eq!(&v[..], &[1, 2, 3, 4]);
+	// Test for Deref implementation
 	assert_eq!({ let sl :&[_] = &v; sl }, &[1, 2, 3, 4]);
 }
