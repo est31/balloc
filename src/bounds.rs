e56a44926869d26ae32300d422f4f1f9fcc78256
est31 <MTest31@outlook.com>
2019-03-09 13:48:08 +0100

    Also impl for Rc<Cell<B>>
    This is cheaper to do overall :)

--- src/bounds.rs
+++ src/bounds.rs
@@ -1,6 +1,6 @@
 use super::AllocError;
 use std::rc::Rc;
-use std::cell::RefCell;
+use std::cell::{RefCell, Cell};
 
 pub trait AllocBound {
 	fn try_alloc(&mut self, amount :usize) -> Result<(), AllocError>;
@@ -19,7 +19,7 @@ impl AllocBound for Unbounded {
 	}
 }
 
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
 pub struct NumberBounded(usize);
 
 impl NumberBounded {
@@ -54,5 +54,20 @@ impl<B :AllocBound> AllocBound for Rc<RefCell<B>> {
 	fn dealloc(&mut self, amount :usize) {
 		let b :&mut B = &mut *self.borrow_mut();
 		b.dealloc(amount);
+	}
+}
+
+
+impl<B :AllocBound + Copy> AllocBound for Rc<Cell<B>> {
+	fn try_alloc(&mut self, amount :usize) -> Result<(), AllocError> {
+		let mut val = self.get();
+		try!(val.try_alloc(amount));
+		self.set(val);
+		Ok(())
+	}
+	fn dealloc(&mut self, amount :usize) {
+		let mut val = self.get();
+		val.dealloc(amount);
+		self.set(val);
 	}
 }
